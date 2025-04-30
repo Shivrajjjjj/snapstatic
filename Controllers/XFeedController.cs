a3c12d43c12eac7300c1d5d17aabb6d2312f0b47
Shivraj Randive <shivrajrandive9281@gmail.com>
2025-04-30 20:06:32 +0530

    Update XFeedController.cs

--- Controllers/XFeedController.cs
+++ Controllers/XFeedController.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using Microsoft.AspNetCore.Mvc;
 using StaticSite.Models;
 
@@ -24,30 +23,3 @@ public class XFeedController : ControllerBase
         return CreatedAtAction(nameof(Get), new { id = post.Id }, post);
     }
 }
-=======
-﻿using Microsoft.AspNetCore.Mvc;
-using StaticSite.Models;
-
-[Route("api/[controller]")]
-[ApiController]
-public class XFeedController : ControllerBase
-{
-    private static List<XFeedPost> _posts = new List<XFeedPost>();
-
-    // GET: api/XFeed
-    [HttpGet]
-    public IEnumerable<XFeedPost> Get()
-    {
-        return _posts;
-    }
-
-    // POST: api/XFeed
-    [HttpPost]
-    public ActionResult<XFeedPost> Post([FromBody] XFeedPost post)
-    {
-        post.CreatedAt = DateTime.UtcNow;
-        _posts.Add(post);
-        return CreatedAtAction(nameof(Get), new { id = post.Id }, post);
-    }
-}
->>>>>>> 5f85b853791a9fa436df4ac7a04582cd2b4408d1
