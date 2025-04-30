f50d1b07c363e85add28ba8de1ebf6e8cec6819e
Shivraj Randive <shivrajrandive9281@gmail.com>
2025-04-30 20:05:56 +0530

    Update ThreadsController.cs

--- Controllers/ThreadsController.cs
+++ Controllers/ThreadsController.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using Microsoft.AspNetCore.Mvc;
 using StaticSite.Models;
 
@@ -24,29 +23,7 @@ public class ThreadsController : ControllerBase
         return CreatedAtAction(nameof(Get), new { id = post.Id }, post);
     }
 }
-=======
-﻿using Microsoft.AspNetCore.Mvc;
-using StaticSite.Models;
 
-[Route("api/[controller]")]
-[ApiController]
-public class ThreadsController : ControllerBase
-{
-    private static List<ThreadPost> _posts = new List<ThreadPost>();
-
-    // GET: api/Threads
-    [HttpGet]
-    public IEnumerable<ThreadPost> Get()
-    {
-        return _posts;
-    }
-
-    // POST: api/Threads
-    [HttpPost]
-    public ActionResult<ThreadPost> Post([FromBody] ThreadPost post)
-    {
-        post.CreatedAt = DateTime.UtcNow;
-        _posts.Add(post);
         return CreatedAtAction(nameof(Get), new { id = post.Id }, post);
     }
 }
